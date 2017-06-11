576c7fbd767d1fb5d6a20d21e924b542202762ac
fins <baeeeh@gmail.com>
2017-06-11 20:43:19 +0200

    Test

---
+++ network.cpp
@@ -0,0 +1,5 @@
+//
+// Created by baeh on 11.06.2017.
+//
+
+#include "network.h"
---
+++ network.h
@@ -0,0 +1,14 @@
+//
+// Created by baeh on 11.06.2017.
+//
+
+#ifndef SRV_CODING_NETWORK_H
+#define SRV_CODING_NETWORK_H
+
+
+class control {
+
+};
+
+
+#endif //SRV_CODING_NETWORK_H
